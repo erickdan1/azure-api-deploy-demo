461a6b2133a5e39d9958f911d6b4bf8ae6b2244c
Erick Daniel <erickdanielalveslima@gmail.com>
2025-01-30 02:16:32 -0300

    feat: add temperatura em Kelvin na response

--- Program.cs
+++ Program.cs
@@ -41,4 +41,6 @@ app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public int TemperatureK => 273 + TemperatureC;
 }
